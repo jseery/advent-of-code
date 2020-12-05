ff2699541c79dca0de36d400cd860fb27abe1a2c
Jonathan Seery <seery.jonathan@gmail.com>
2020-12-04 22:37:38 -0500

    first commit

---
+++ advent_of_code/day_1/day_1.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import itertools
+from functools import reduce
+
+def find_items_sum_to_2020(filename: str, tuple_size: int):
+
+    input = map(int, Path(__file__).parent.joinpath(filename).read_text().splitlines())
+
+    for combo in itertools.combinations(input, tuple_size):
+        if sum(combo) == 2020:
+            print(combo)
+            answer = reduce(lambda x, y: x * y, combo)
+            print(answer)
+
+    return answer
+
+def main():
+
+    fname = 'input.txt'
+
+    find_items_sum_to_2020(fname, 2)
+
+    find_items_sum_to_2020(fname, 3)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ advent_of_code/day_2/day_2.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Tuple
+from collections import Counter
+
+import re
+
+from dataclasses import dataclass
+
+@dataclass
+class PolicyPW(object):
+    min: str
+    max: str
+    char: str
+    pw: str
+
+    def has_valid_pw(self):
+        self.char_count = Counter(self.pw).get(self.char)
+        if not self.char_count:
+            return False
+        elif int(self.char_count) < int(self.min):
+            return False
+        elif int(self.char_count) > int(self.max):
+            return False
+        else:
+            return True
+
+
+def parse_password(pw_string: str) -> PolicyPW:
+    expr = re.compile(r'(?P<min>[\d]+)-(?P<max>[\d]+) (?P<char>[a-z]): (?P<pw>[a-z]+)')
+    output = PolicyPW(**expr.match(pw_string).groupdict())
+    return output
+
+def get_puzzle_input(fname: str):
+    input = Path(__file__).parent.joinpath(fname).read_text()
+    return input.splitlines()
+
+def part_one():
+    input = get_puzzle_input('input.txt')
+
+    parsed_pws = list(map(parse_password, input))
+    valid_pw = filter(lambda x: x.has_valid_pw(), parsed_pws)
+    broken_pw = filter(lambda x: not x.has_valid_pw(), parsed_pws)
+    answer = len(list(valid_pw))
+    print(f"{answer=}")
+
+    return
+
+
+@dataclass
+class PolicyPartTwo(object):
+    pos_first: str
+    pos_second: str
+    char: str
+    pw: str
+
+    def has_valid_pw(self):
+        idx_first = int(self.pos_first)
+        idx_second = int(self.pos_second)
+
+        char_first = self.pw[idx_first-1]
+        char_second = self.pw[idx_second-1]
+
+        if bool(char_first == self.char) ^ bool(char_second == self.char):
+            return True
+        else:
+            return False
+
+
+def parse_part_two(pw_string: str) -> PolicyPartTwo:
+    expr = re.compile(r'(?P<pos_first>[\d]+)-(?P<pos_second>[\d]+) (?P<char>[a-z]): (?P<pw>[a-z]+)')
+    output = PolicyPartTwo(**expr.match(pw_string).groupdict())
+    return output
+
+
+def part_two():
+
+    input_list = get_puzzle_input('input.txt')
+
+    parsed_pws = list(map(parse_part_two, input_list))
+
+    valid_pw = filter(lambda x: x.has_valid_pw(), parsed_pws)
+    broken_pw = filter(lambda x: not x.has_valid_pw(), parsed_pws)
+    answer = len(list(valid_pw))
+    print(f"{answer=}")
+
+    return
+
+def main():
+    part_two()
+
+
+if __name__ == "__main__":
+    main()
