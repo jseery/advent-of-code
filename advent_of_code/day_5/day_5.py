b7a159cf6087fa0ad3c4a8d7235bc36e4302ab6c
Jonathan Seery <seery.jonathan@gmail.com>
2020-12-11 22:35:46 -0500

    finished day 5

--- advent_of_code/day_5/day_5.py
+++ advent_of_code/day_5/day_5.py
@@ -1,9 +1,12 @@
 from pathlib import Path
+import itertools
 
 
 ROWS = 128
+ROW_PARTITIONS = [64, 32, 16, 8, 4, 2, 1]
+COLS = 8
+COL_PARTITIONS = [4, 2, 1]
 
-PARTITIONS = [64, 32, 16, 8, 4, 2, 1]
 
 
 def get_puzzle_input(fname: str):
@@ -13,31 +16,52 @@ def get_puzzle_input(fname: str):
 
 
 def find_row(seq: str) -> int:
-    print(seq)
+    assert len(seq) == 7
     remaining_rows = list(range(ROWS))
-    for p in zip(seq, PARTITIONS):
-        print(p)
+    for s, p in zip(seq, ROW_PARTITIONS):
         if s == 'F':
             remaining_rows = remaining_rows[:p]
-            print(remaining_rows)
         elif s == 'B':
             remaining_rows = remaining_rows[p:]
     assert len(remaining_rows) == 1, f"{remaining_rows=}"
-    print(remaining_rows)
-    return remaining_rows
+    return remaining_rows[0]
+
+
+def find_col(seq: str) -> int:
+    assert len(seq) == 3
+    remaining_cols = list(range(COLS))
+    for s, p in zip(seq, COL_PARTITIONS):
+        if s == 'L':
+            remaining_cols = remaining_cols[:p]
+        elif s == 'R':
+            remaining_cols = remaining_cols[p:]
+    assert len(remaining_cols) == 1, f"{remaining_cols=}"
+    return remaining_cols[0]
+
+
+def find_seat_id(p: str) -> int:
+    found_row = find_row(p[:7])
+    found_col = find_col(p[7:])
+    seat_id = (found_row * 8) + found_col
+    # print(f"{found_row=}, {found_col=}, {seat_id=}")
+    return seat_id
+
 
 
 def main():
 
-    pz = get_puzzle_input("test.txt")
-    print(pz)
+    pz = get_puzzle_input("input.txt")
 
-    rows = map(lambda l: l[:7], pz)
-    print(list(rows))
+    taken_seat_ids = [find_seat_id(p) for p in pz]
+    taken_seat_ids.sort()
+    answer_1 = max(taken_seat_ids)
+    print(answer_1)
 
-    for r in rows:
-        found = find_row(r)
-        print(found)
+
+    all_seats = list(range(min(taken_seat_ids), answer_1))
+
+    answer_2 = set(all_seats) - set(taken_seat_ids)
+    print(f"{answer_2=}")
 
     return
 
